f63e49c5288cafc427a87476019871415ab9dc53
Ohad Nave <ohadnave@gmail.com>
2025-08-26 22:28:24 +0300

    nake book card grid or list

--- components/BookCard.tsx
+++ components/BookCard.tsx
@@ -1,39 +1,77 @@
-import { View, Text, Image, StyleSheet, TouchableOpacity } from "react-native";
-import colors from "@config/colors";
+import { Image, StyleSheet, TouchableOpacity } from "react-native";
 import React from "react";
+import useColors from "@hooks/useColors";
+import Text from "./Text";
+import { useSelector } from "react-redux";
 
 interface BookCardProps {
   title: string;
   releaseDate: string;
   cover: string;
-  onPress: () => void;
+  onPress?: () => void;
 }
 
 const BookCard = ({ title, releaseDate, cover, onPress }: BookCardProps) => {
+  const listType = useSelector((state: any) => state.filters.listType);
+  const colors = useColors();
+
+  if (listType === "list") {
+    return (
+      <TouchableOpacity
+        style={[styles.listItem, { backgroundColor: colors.light }]}
+        onPress={onPress}
+        activeOpacity={0.8}
+      >
+        <Image
+          source={{ uri: cover }}
+          style={styles.listCover}
+          resizeMode="contain"
+        />
+        <Text
+          numberOfLines={2}
+          style={[styles.listTitle, { color: colors.black }]}
+        >
+          {title}
+        </Text>
+        <Text style={[styles.listDate, { color: colors.darkMedium }]}>
+          {releaseDate}
+        </Text>
+      </TouchableOpacity>
+    );
+  }
+
   return (
-    <TouchableOpacity style={styles.card} onPress={onPress}>
+    <TouchableOpacity
+      style={[
+        styles.card,
+        { backgroundColor: colors.light, borderColor: colors.medium },
+      ]}
+      onPress={onPress}
+      activeOpacity={0.8}
+    >
       <Image
         source={{ uri: cover }}
         style={styles.cover}
         resizeMode="contain"
       />
-      <Text style={styles.title}>{title}</Text>
-      <Text style={styles.date}>{releaseDate}</Text>
+      <Text numberOfLines={3} style={styles.title}>
+        {title}
+      </Text>
+      <Text style={[styles.date, { color: colors.darkMedium }]}>
+        {releaseDate}
+      </Text>
     </TouchableOpacity>
   );
 };
 
 const styles = StyleSheet.create({
   card: {
-    backgroundColor: colors.white,
-    borderRadius: 8,
-    padding: 12,
+    borderWidth: 1,
+    paddingVertical: 8,
+    paddingHorizontal: 4,
     alignItems: "center",
-    shadowColor: colors.black,
-    shadowOpacity: 0.1,
-    shadowRadius: 4,
-    elevation: 2,
     margin: 8,
+    height: 260,
     width: "90%",
   },
   cover: {
@@ -45,12 +83,36 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     fontSize: 14,
     textAlign: "center",
-    marginBottom: 2,
-    color: colors.black,
+    marginBottom: 4,
   },
   date: {
-    color: colors.darkMedium,
     fontSize: 12,
+  },
+  listItem: {
+    width: "100%",
+    flexDirection: "row",
+    alignItems: "center",
+    paddingVertical: 10,
+    paddingHorizontal: 16,
+    marginVertical: 4,
+    borderRadius: 0,
+  },
+  listCover: {
+    width: 60,
+    height: 90,
+    marginRight: 14,
+  },
+  listTitle: {
+    flex: 1,
+    fontWeight: "bold",
+    fontSize: 15,
+    marginBottom: 2,
+    textAlign: "left",
+  },
+  listDate: {
+    fontSize: 13,
+    marginLeft: 8,
+    alignSelf: "flex-start",
   },
 });
 
