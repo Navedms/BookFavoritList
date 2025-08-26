5534bcf454ed516c9cec71190ed9dade73dcb4a0
Ohad Nave <ohadnave@gmail.com>
2025-08-26 18:17:14 +0300

    add initial config style

---
+++ config/colors.tsx
@@ -0,0 +1,24 @@
+export interface Colors {
+  [key: string]: string;
+}
+
+const colors: Colors = {
+  opacity: "rgba(255, 255, 255, 0)",
+  black: "#000",
+  opacityBlack: "rgba(0, 0, 0, 0.5)",
+  white: "#fff",
+  background: "#fff",
+  opacityWhite: "rgba(255, 255, 255, 0.8)",
+  primary: "#000099",
+  secondary: "#6666ff",
+  dark: "#5a5a5a",
+  darkMedium: "#a6a6a6",
+  medium: "#d9d9d9",
+  light: "#f2f2f2",
+  delete: "#ff3333",
+  lightDelete: "#FF7D7D",
+  sun: "#fff12e",
+  disabled: "#F2DEDE",
+};
+
+export default colors;
