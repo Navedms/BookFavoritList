54b157b3c7ebb68334d2b8ac34965941dc69bfd9
Ohad Nave <ohadnave@gmail.com>
2025-08-26 19:15:26 +0300

    add inputs and select componenets for filters - form componenets

---
+++ components/forms/ErrorMessage.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { StyleSheet } from "react-native";
+
+import colors from "@config/colors";
+import Text from "../Text";
+
+interface ErrorMessageProps {
+  error: string;
+  visible: boolean;
+  style?: any;
+}
+
+function ErrorMessage({ error, visible, style }: ErrorMessageProps) {
+  if (!visible || !error) return null;
+
+  return <Text style={[styles.error, style]}>{error}</Text>;
+}
+
+const styles = StyleSheet.create({
+  error: {
+    color: colors.delete,
+    textAlign: "center",
+  },
+});
+
+export default ErrorMessage;
---
+++ components/forms/index.tsx
@@ -0,0 +1,5 @@
+export { default as Form } from "./Form";
+export { default as FormField } from "./FormField";
+export { default as FormGroupPicker } from "./FormGroupPicker";
+export { default as ErrorMessage } from "./ErrorMessage";
+export { default as SubmitButton } from "./SubmitButton";
