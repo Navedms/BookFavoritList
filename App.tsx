896681c2cbcd4f287ac217def28f6f957d5b0dd0
Ohad Nave <ohadnave@gmail.com>
2025-08-27 13:12:02 +0300

    add splash screen and icon

--- App.tsx
+++ App.tsx
@@ -1,9 +1,10 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { persistStore } from "redux-persist";
 import { NavigationContainer } from "@react-navigation/native";
+import * as SplashScreen from "expo-splash-screen";
 import FlashMessage from "react-native-flash-message";
 
 // store
@@ -13,22 +14,34 @@ import { store } from "@store/store";
 import { navigationRef } from "@navigation/rootNavigation";
 import { getNavigationTheme } from "@navigation/navigationTheme";
 import AppNavigator from "@navigation/AppNavigator";
+import AppSplashScreen from "@components/AppSplashScreen";
 
 const queryClient = new QueryClient();
 
 export default function App() {
+  const [isReady, setIsReady] = useState(false);
+
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   const persistor = persistStore(store);
   return (
     <Provider store={store}>
       <PersistGate persistor={persistor}>
         <QueryClientProvider client={queryClient}>
-          <NavigationContainer
-            ref={navigationRef}
-            theme={getNavigationTheme("light")}
-          >
-            <AppNavigator />
-            <FlashMessage position="bottom" />
-          </NavigationContainer>
+          {isReady ? (
+            <NavigationContainer
+              ref={navigationRef}
+              theme={getNavigationTheme("light")}
+            >
+              <AppNavigator />
+            </NavigationContainer>
+          ) : (
+            <AppSplashScreen setIsReady={setIsReady} />
+          )}
+
+          <FlashMessage position="bottom" />
         </QueryClientProvider>
       </PersistGate>
     </Provider>
